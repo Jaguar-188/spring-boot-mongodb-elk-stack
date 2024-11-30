80d00fceed9ef459f03c40eabda8f05439c92a62
Jaguar-188 <ruturajlokhande9@gmail.com>
2024-11-30 18:52:47 +0530

    master || Ruturaj || created a method at repo layer to fetch data using requested id

--- src/main/java/com/example/mdbspringboot/repository/MongoStudentRepository.java
+++ src/main/java/com/example/mdbspringboot/repository/MongoStudentRepository.java
@@ -3,11 +3,18 @@ package com.example.mdbspringboot.repository;
 import com.example.mdbspringboot.model.entity.Student;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface MongoStudentRepository extends MongoRepository<Student,String> {
 
     @Query(value="{name:'?0'}")
-    Student findAll(String name);
+    Student findByName(String name);
+
+    @Query(value ="{id:'?0'}")
+    Student findByUserId(String id);
 
 //    @Query(value="{name:'?0'}")
 //    List<Student> findAll(String name);
