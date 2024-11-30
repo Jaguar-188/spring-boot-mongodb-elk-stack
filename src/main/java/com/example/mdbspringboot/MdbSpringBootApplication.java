7abd0a4cb65dc4671167f470db6c875d9a985b49
Jaguar-188 <ruturajlokhande9@gmail.com>
2024-11-30 18:53:48 +0530

    master || Ruturaj || enabled caching

--- src/main/java/com/example/mdbspringboot/MdbSpringBootApplication.java
+++ src/main/java/com/example/mdbspringboot/MdbSpringBootApplication.java
@@ -3,10 +3,12 @@ package com.example.mdbspringboot;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EnableMongoRepositories
+@EnableCaching
 public class MdbSpringBootApplication {
 
     public static void main(String[] args) {
