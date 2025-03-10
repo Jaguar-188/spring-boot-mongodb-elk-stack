75818753e6853d13b4a1ef2348b6f69f96330282
Jaguar-188 <ruturajlokhande9@gmail.com>
2025-03-11 00:51:32 +0530

    changed findByName method to return list instead single entity

--- src/main/java/com/example/mdbspringboot/repository/MongoStudentRepository.java
+++ src/main/java/com/example/mdbspringboot/repository/MongoStudentRepository.java
@@ -5,13 +5,14 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface MongoStudentRepository extends MongoRepository<Student,String> {
 
     @Query(value="{name:'?0'}")
-    Student findByName(String name);
+    List<Student> findByName(String name);
 
     @Query(value ="{id:'?0'}")
     Student findByUserId(String id);
