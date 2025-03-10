657ecb256b9666b4d7d60247b9ed2666e9f5d270
Jaguar-188 <ruturajlokhande9@gmail.com>
2025-03-11 00:48:50 +0530

    created meaningfull exception message

--- src/main/java/com/example/mdbspringboot/exceptions/UserNotFoundException.java
+++ src/main/java/com/example/mdbspringboot/exceptions/UserNotFoundException.java
@@ -3,7 +3,7 @@ package com.example.mdbspringboot.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.NOT_FOUND)
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "User Not Found")
 public class UserNotFoundException extends RuntimeException {
 
     public UserNotFoundException(String message) {
